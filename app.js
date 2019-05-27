d860b5cf3b9acc73806bf1a719fbeb98542f116d
Anmol Pandey <anmol@classpro.in>
2019-05-27 16:43:39 +0530

    added status quote added

--- app.js
+++ app.js
@@ -10,8 +10,12 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 Page not found</h1>');
+});
 
 const server = http.createServer(app);
 server.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
   console.log('Inside express');
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+    '<form action="/admin/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
   );
 });
 
+// /admin/product => POST
 router.post('/product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
