6f873400ce41505bd8800f9b482d5992ccb76eb4
Anmol Pandey <anmol@classpro.in>
2019-05-31 18:18:24 +0530

    added ejs, pug and express-handlebars package

--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ const express = require('express');
 const rootDir = require('../util/path');
 
 const router = express.Router();
+const products = [];
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
@@ -13,8 +14,9 @@ router.get('/add-product', (req, res, next) => {
 
 // /admin/add-product => POST
 router.post('/add-product', (req, res, next) => {
-  console.log(req.body);
+  products.push({ title: req.body.title });
   res.redirect('/');
 });
 
-module.exports = router;
+exports.routes = router;
+exports.products = products;
