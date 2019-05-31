561640d5ff1ee093e0a2d31804be70962092c4a9
Anmol Pandey <anmol@classpro.in>
2019-05-31 17:40:49 +0530

    changed files to maximillians

--- app.js
+++ app.js
@@ -1,26 +1,21 @@
-const http = require('http');
+const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const path = require('path');
+
 const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-//It is use to parse the responses
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//It is used to serve files statically to webpages
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-//routes
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-const server = http.createServer(app);
-server.listen(3000);
+app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,18 @@
-const express = require('express');
-const router = express.Router();
 const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
 
 // /admin/add-product => GET
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-// /admin/product => POST
-router.post('/product', (req, res, next) => {
+// /admin/add-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
