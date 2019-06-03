c254e2d70d0ba7fbf43d667adaa0f2d34eae9ad5
Anmol Pandey <anmol@classpro.in>
2019-06-03 18:17:48 +0530

    created model

--- controllers/products_controller.js
+++ controllers/products_controller.js
@@ -1,16 +1,18 @@
+const Product = require('../models/product');
 exports.getAddProductPage = (req, res, next) => {
   res.render('add-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product'
   });
 };
-const products = [];
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', { products, pageTitle: 'Shop', path: '/' });
 };
