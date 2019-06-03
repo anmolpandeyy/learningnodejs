3e98916847b5e64d8fc9f31cf10ff05deef805a5
Anmol Pandey <anmol@classpro.in>
2019-06-03 16:28:44 +0530

    implemented EJS and setup base project with partials

--- routes/shop.js
+++ routes/shop.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', { products, pageTitle: 'Shop' });
+  res.render('shop', { products, pageTitle: 'Shop', path: '/' });
 });
 
 module.exports = router;
