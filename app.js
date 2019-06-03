4ac277b4c7566880599db8e0bf85c8f6c8ae6e6f
Anmol Pandey <anmol@classpro.in>
2019-06-03 17:13:13 +0530

    finished learning controllers

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const { get404 } = require('./controllers/404_controller');
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -17,8 +17,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(get404);
 
 app.listen(3000);
