9b0538b46b538cc1e93c0ccc7de5409a30a1b272
Anmol Pandey <anmol@classpro.in>
2019-06-04 18:56:17 +0530

    Done

--- controllers/shop.js
+++ controllers/shop.js
@@ -10,6 +10,17 @@ exports.getProducts = (req, res, next) => {
   });
 };
 
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.productId;
+  Product.findById(productId, product => {
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
+
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('shop/index', {
