63cb207ebb1d331af9bfc5d5716fef9356346dbe
Anmol Pandey <anmol@classpro.in>
2019-06-06 16:04:29 +0530

    Added add to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
@@ -11,8 +12,8 @@ exports.getProducts = (req, res, next) => {
 };
 
 exports.getProduct = (req, res, next) => {
-  const productId = req.params.productId;
-  Product.findById(productId, product => {
+  const prodId = req.params.productId;
+  Product.findById(prodId, product => {
     res.render('shop/product-detail', {
       product: product,
       pageTitle: product.title,
@@ -36,6 +37,14 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  Product.findById(prodId, product => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
--- models/product.js
+++ models/product.js
@@ -26,7 +26,7 @@ module.exports = class Product {
   }
 
   save() {
-    this.id = new Date().getTime().toString();
+    this.id = Math.random().toString();
     getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
@@ -41,7 +41,7 @@ module.exports = class Product {
 
   static findById(id, cb) {
     getProductsFromFile(products => {
-      const product = products.find(item => item.id === id);
+      const product = products.find(p => p.id === id);
       cb(product);
     });
   }
